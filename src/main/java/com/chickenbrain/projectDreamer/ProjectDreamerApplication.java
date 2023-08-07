e1d8f1fcf1459adcbb787e02863d2d8d4def58ca
hamletSolanoD <hamletsolanod@gmail.com>
2023-08-06 19:13:15 -0600

    springboot

---
+++ src/main/java/com/chickenbrain/projectDreamer/ProjectDreamerApplication.java
@@ -0,0 +1,13 @@
+package com.chickenbrain.projectDreamer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectDreamerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectDreamerApplication.class, args);
+	}
+
+}
